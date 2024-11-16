24f0b45faa359c34b7ce883bf5b85e6ef7d4da8a
austin <dhtjstlr777@gmail.com>
2024-11-17 01:46:47 +0900

    module 내에서 사용할 repository inject

--- src/modules/seller/seller.module.ts
+++ src/modules/seller/seller.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SellerService } from './seller.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Seller } from './entities/seller.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Seller])],
   providers: [SellerService],
 })
 export class SellerModule {}
